db90242b86ed0bda76ce2474f09e9bba747a6e3d
Rafael Klaessen <rafaelklaessen.nl@gmail.com>
2018-08-11 16:27:18 +0200

    Dialog now closes when esc is pressed

--- src/components/Dialog/index.js
+++ src/components/Dialog/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import DialogWrapper from './DialogWrapper';
 import DialogOverlay from './DialogOverlay';
@@ -9,61 +9,76 @@ import DialogTitle from './DialogTitle';
 import DialogContent from './DialogContent';
 import DialogActions from './DialogActions';
 
-const Dialog = ({
-  open,
-  width,
-  animation,
-  wrapperProps,
-  overlayProps,
-  closeButtonProps,
-  onRequestClose,
-  children,
-  ...props
-}) => (
-  <DialogWrapper open={open} {...wrapperProps}>
-    <DialogOverlay
-      open={open}
-      onClick={onRequestClose}
-      noAnimation={animation === Animations.NONE}
-      {...overlayProps}
-    />
-    <StyledDialog open={open} width={width} animation={animation} {...props}>
-      <CloseButton onClick={onRequestClose} {...closeButtonProps} />
-      {children}
-    </StyledDialog>
-  </DialogWrapper>
-);
+export default class Dialog extends Component {
+  static propTypes = {
+    open: PropTypes.bool,
+    width: PropTypes.oneOfType([PropTypes.number, PropTypes.string]),
+    animation: PropTypes.oneOf([
+      Animations.SLIDE,
+      Animations.GROW,
+      Animations.FADE,
+      Animations.NONE
+    ]),
+    wrapperProps: PropTypes.object,
+    overlayProps: PropTypes.object,
+    closeButtonProps: PropTypes.object,
+    onRequestClose: PropTypes.func,
+    children: PropTypes.node
+  };
 
-Dialog.propTypes = {
-  open: PropTypes.bool,
-  width: PropTypes.oneOfType([PropTypes.number, PropTypes.string]),
-  animation: PropTypes.oneOf([
-    Animations.SLIDE,
-    Animations.GROW,
-    Animations.FADE,
-    Animations.NONE
-  ]),
-  wrapperProps: PropTypes.object,
-  overlayProps: PropTypes.object,
-  closeButtonProps: PropTypes.object,
-  onRequestClose: PropTypes.func,
-  children: PropTypes.node
-};
+  static defaultProps = {
+    open: false,
+    width: 480,
+    animation: Animations.SLIDE,
+    wrapperProps: null,
+    overlayProps: null,
+    closeButtonProps: null
+  };
 
-Dialog.defaultProps = {
-  open: false,
-  width: 480,
-  animation: Animations.SLIDE,
-  wrapperProps: null,
-  overlayProps: null,
-  closeButtonProps: null
-};
+  static Label = DialogLabel;
+  static Title = DialogTitle;
+  static Content = DialogContent;
+  static Actions = DialogActions;
 
-Dialog.Label = DialogLabel;
-Dialog.Title = DialogTitle;
-Dialog.Content = DialogContent;
-Dialog.Actions = DialogActions;
+  static Animations = Animations;
 
-Dialog.Animations = Animations;
+  componentDidMount = () =>
+    document.addEventListener('keydown', this.handleKeyDown);
 
-export default Dialog;
+  componentWillUnmount = () =>
+    document.removeEventListener('keydown', this.handleKeyDown);
+
+  handleKeyDown = (e) => {
+    const code = e.keyCode || e.which;
+    if (code !== 27) return;
+    this.props.onRequestClose(e);
+  };
+
+  render = () => {
+    const {
+      open,
+      width,
+      animation,
+      wrapperProps,
+      overlayProps,
+      closeButtonProps,
+      onRequestClose,
+      children,
+      ...props
+    } = this.props;
+   return (
+      <DialogWrapper open={open} {...wrapperProps}>
+        <DialogOverlay
+          open={open}
+          onClick={onRequestClose}
+          noAnimation={animation === Animations.NONE}
+          {...overlayProps}
+        />
+        <StyledDialog open={open} width={width} animation={animation} {...props}>
+          <CloseButton onClick={onRequestClose} {...closeButtonProps} />
+          {children}
+        </StyledDialog>
+      </DialogWrapper>
+    );
+  };
+}
